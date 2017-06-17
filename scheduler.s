b2ecd5bef1361a272d574d13fd8df7848b013d70
Alon Blumenreich <alon@mail.com>
2017-06-18 01:38:20 +0300

    still working

--- scheduler.s
+++ scheduler.s
@@ -2,11 +2,24 @@
         extern resume, end_co
 
 
+sys_write:      equ   4
+stdout:         equ   1
+
+section .data
+
+hello:  db 'scheduler', 10
+
 section .text
 
 scheduler:
         mov ebx, 1
 .next:
+        ;mov eax, sys_write
+        ;mov ebx, stdout
+        ;mov ecx, hello
+        ;mov edx, 9
+        ;int 80h
+
         call resume             ; resume printer
         loop .next
 
